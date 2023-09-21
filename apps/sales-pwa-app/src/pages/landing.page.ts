be01ec43bdbbbccede3ced455c815c8d2444be62
Austin Ma <austin.ma@oracle.com>
2023-09-21 15:14:51 -0700

    add monorepo test config, vitest dependencies and prettier

--- apps/sales-pwa-app/src/pages/landing.page.ts
+++ apps/sales-pwa-app/src/pages/landing.page.ts
@@ -7,31 +7,33 @@ const ELEMENT_NAME = `${APPLICATION_NAME_SPACE}-landing-page`;
 
 @customElement(ELEMENT_NAME)
 export class LandingPage extends LitElement {
-	render() {
-		return html`
-			<section>
-				<h1>Hello on Sales PWA Landing Page!!!</h1>
-				<button @click=${() => this._onClick()}>Log</button>
-				<av-badge variant="success">Success</av-badge>
-				<av-badge variant="danger">Danger</av-badge>
-				<av-badge variant="default">Default</av-badge>
-			</section>
-		`;
-	}
+  render() {
+    return html`
+      <section>
+        <h1>Hello on Sales PWA Landing Page!!!</h1>
+        <button id="logger-btn" @click=${() => this._onClick()}>Log</button>
+        <av-badge variant="success">Success</av-badge>
+        <av-badge variant="danger">Danger</av-badge>
+        <av-badge variant="default">Default</av-badge>
+      </section>
+    `;
+  }
 
-	private _onClick() {
-		Logger.log('Hello World!');
-		Logger.alert('Hello World!');
-	}
+  private _onClick() {
+    const msg = 'Hello World!';
+    Logger.log(msg);
+    Logger.alert(msg);
+    this.dispatchEvent(new CustomEvent('logger', { detail: msg }));
+  }
 
-	static styles = css`
-		:host {
-		}
-	`;
+  static styles = css`
+    :host {
+    }
+  `;
 }
 
 declare global {
-	interface HTMLElementTagNameMap {
-		ELEMENT_NAME: LandingPage;
-	}
+  interface HTMLElementTagNameMap {
+    ELEMENT_NAME: LandingPage;
+  }
 }
--- apps/sales-pwa-app/vite.config.js
+++ apps/sales-pwa-app/vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	root: '.',
-	build: {
-		outDir: './dist',
-		rollupOptions: {
-			external: [],
-		},
-	},
+  root: '.',
+  build: {
+    outDir: './dist',
+    rollupOptions: {
+      external: [],
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
 });
