c09f3caf109a23c6f7393f8005f072c0d17ac6c6
Austin Ma <austin.ma@oracle.com>
2024-01-10 13:11:50 -0800

    feat: first take on text fields web component

--- apps/sales-pwa-app/src/pages/login.page.ts
+++ apps/sales-pwa-app/src/pages/login.page.ts
@@ -9,7 +9,26 @@ export class LoginPage extends ApplicationView {
       <section>
         <avx-login-form></avx-login-form>
       </section>
+      <form @submit=${this.onSubmitHandler}>
+        <avx-text-input
+          style="padding: 100px"
+          label="name"
+          type="text"
+          name="name"
+        ></avx-text-input>
+        <avx-button type="outlined" label="Sign In" behavior="submit"></avx-button>
+      </form>
     `;
+  }
+
+  onSubmitHandler(e) {
+    e.preventDefault();
+    const form = e.target as HTMLFormElement;
+    const formData = new FormData(form);
+    // for testing purposes currently
+    for (const pair of formData.entries()) {
+      console.log(`${pair[0]}, ${pair[1]}`);
+    }
   }
 }
 
